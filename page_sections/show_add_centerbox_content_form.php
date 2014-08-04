8a8b01dc7fbedcb6c9a71c23aebcd9c0cda382f3
Mahder Neway <mahder.neway@echoorigin.com>
2014-08-04 17:34:45 -0400

    designed the admin center box content entry form

---
+++ page_sections/show_add_centerbox_content_form.php
@@ -0,0 +1,37 @@
+<div class="panel-body">
+    <div class="tab-pane fade in active">
+        <h4>Add Center Box Content</h4>
+        <!--the first tab to load is the username setting form...-->                                                            
+        <form role="form">            
+            <div class="form-group">        
+                <label>Title</label>
+                <input ng-model="centerBox.title" class="form-control" id="title" type="text" placeholder="Enter the title..." required=""/>
+            </div>            
+            <div class="form-group">
+                <label>The Content</label>
+                <textarea ng-model="centerBox.content" 
+                class="form-control jqte-test" id="content" required=""></textarea>
+            </div>
+            <div class="error-text">
+                {{message}}
+            </div>
+            <button type="button" class="btn btn-primary" ng-click="sendMail();">Save
+            </button>
+            <button type="reset" class="btn btn-primary">Reset</button>    
+        </form>
+        
+    </div>
+</div>
+<script type="text/javascript">
+    jQuery(document).ready(function(){
+        $('.jqte-test').jqte();         
+        // settings of status
+        var jqteStatus = true;
+        $(".status").click(function()
+        {
+            jqteStatus = jqteStatus ? false : true;
+            $('.jqte-test').jqte({"status" : jqteStatus})
+        });
+    });//end document.ready function
+</script>
+
